893a2904ccb3798322225cc97d1b551f8eef119b
unknown <m.gontarek@wrseti.pl>
2022-11-06 20:30:38 +0100

    Add continous deployment

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask.wrappers import Response
+import git
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./FaceID')
+    origin = repo.remotes.origin
+    repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    origin.pull()
+    return '', 200
+    
 @app.route("/", methods=['GET'])
 def home():
     return "Hello world!"
