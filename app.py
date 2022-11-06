7f861df95fa1426d4a8f29db6f921488d57da5d1
unknown <m.gontarek@wrseti.pl>
2022-11-06 20:52:05 +0100

    Delete webhook

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask
 from flask.wrappers import Response
-import git
 
 app = Flask(__name__)
 
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    repo = git.Repo('./FaceID')
-    origin = repo.remotes.origin
-    repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-    origin.pull()
-    return '', 200
-    
 @app.route("/", methods=['GET'])
 def home():
     return "dupa"
