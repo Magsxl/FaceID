021325130784d05d35d6da4122323b00b4c6dd94
unknown <m.gontarek@wrseti.pl>
2022-11-07 17:14:59 +0100

    Integrate flask with flutter

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask
-from flask.wrappers import Response
+from flask import send_from_directory
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def home():
-    return "dupa"
+FLUTTER_WEB_APP = 'templates'
+
+@app.route("/web/", methods=['GET'])
+def render_page_web():
+    return render_template('index.html')
+
+@app.route("/web/<path:name>")
+def return_flutter_doc(name):
+    datalist = str(name).split('/')
+    DIR_NAME = FLUTTER_WEB_APP
+
+    if len(datalist) > 1:
+        for i in range(0, len(datalist) - 1):
+            DIR_NAME += '/' + datalist[i]
+    
+    return send_from_directory(DIR_NAME, datalist[-1])
+
+@app.route("/")
+def render_page():
+    return render_template('/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
